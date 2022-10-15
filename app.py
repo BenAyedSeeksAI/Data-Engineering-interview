c146ad59a33bd9232378f2d85238a1b8a8187727
Mohamed Fares Ben ayed <faresaction@yahoo.fr>
2022-10-15 08:15:48 +0200

    Final commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from model import Arbre
 from config import INTRO
 
 import pandas as pd
+import numpy as np
 import plotly.express as xp
 from dash import Dash, html, dcc
 
@@ -20,6 +21,7 @@ def getProjectedData(columns):
 
 
 def getHauteurMaximumParLibelle():
+    # On a fait une simple aggregation pour manifester les hauteur maximales dans chaque libelle
     data = getProjectedData(["Hauteur", "LibelleFrancais"])
     data = pd.DataFrame(data.groupby("LibelleFrancais",as_index=False).agg(
           MaximumHauteur = ("Hauteur","max")
@@ -43,6 +45,19 @@ def PiePlot():
     pie_plot = dcc.Graph(figure=fig)
     return pie_plot
 
+def PiePlotAdresse():
+    df = getProjectedData(["Adresse"])
+    condition1 = df["Adresse"] == "JARDINS DES CHAMPS ELYSEES - SQUARE MARIGNY / 41 AVENUE GABRIEL"
+    condition2 = df["Adresse"] == "PARC MONTSOURIS / 28 BOULEVARD JOURDAN"
+    condition3 = df["Adresse"] == "ILE DE BERCY / LAC DAUMESNIL"
+    condition4 = df["Adresse"] == "PARC DES BUTTES CHAUMONT"
+    df["Adresse"] = np.where(condition1 | condition2 | condition3 | condition4, df["Adresse"], "others" ) 
+    fig = xp.pie(df,
+                 names="Adresse",
+                 title="Répartition des arbres par quartier")
+    pie_plot_adresse = dcc.Graph(figure=fig)
+    return pie_plot_adresse
+
 def BarPlot():
     df = getHauteurMaximumParLibelle()
     fig = xp.bar(df,
@@ -53,11 +68,12 @@ def BarPlot():
     return bar_plot
 app = Dash(__name__)
 app.layout = html.Div(children=[
-    html.H1(children='Arbre Remarquables dashboard'),
+    html.H1(children="Dashboard d'arbres remarquables"),
     html.Div(children=[html.P(children= INTRO)]),
     ScatterPlot(),
     PiePlot(),
     BarPlot(),
+    PiePlotAdresse(),
 ])
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 URLARBRES = "https://opendata.paris.fr/api/records/1.0/search/?dataset=arbresremarquablesparis&q=&rows=1000&facet=libellefrancais&facet=genre&facet=espece&facet=stadedeveloppement&facet=varieteoucultivar&facet=dateplantation"
 
 INTRO = '''
-        Dans ce dashboard, on a joué sur 4 axes de données : Hauteur, Circonference, Domanialite et LibelleFrancais.
-        Le dashabord est un contenaire HTML qui contient 3 figures graphiques (un scatter plot, un pie plot et enfin un bar plot).   
+        Dans ce dashboard, on a joué sur 5 axes de données : Hauteur, Circonference, Domanialite, Adresse et LibelleFrancais.
+        Le dashabord est un contenaire HTML qui contient 4 figures graphiques (un scatter plot, un pie plot, un bar plot et enfin un pie plot).   
 '''
